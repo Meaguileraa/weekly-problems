7bbce196a2d5105f668c17cdb0899155d6b12037
Marissa Aguilera <marissaguilera@yahoo.com>
2020-11-18 12:15:58 -0800

    max number test cases

---
+++ problem.py
@@ -0,0 +1,11 @@
+
+#This file has the weekly wednesday HB problems
+
+#11/18 
+
+#1. Max Number 
+
+def max_num(num_list):
+    """Returns largest integer from given list"""
+
+print(max_num([5, 3, 6, 2, 1])) #6
