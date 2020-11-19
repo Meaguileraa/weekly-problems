984c128b18faa1c1f0b409e830cb7a7dcc9c5c53
Marissa Aguilera <marissaguilera@yahoo.com>
2020-11-18 16:51:08 -0800

    count list recursively

--- problem.py
+++ problem.py
@@ -3,6 +3,8 @@
 
 #11/18 
 
+####################################################################
+
 #1. Max Number 
 
 def max_num(num_list):
@@ -11,3 +13,25 @@ def max_num(num_list):
     return max(num_list)
 
 print(max_num([5, 3, 6, 2, 1])) #6
+
+
+
+#2. Count List Recursively 
+
+def count_recursively(lst):
+    """Return number of items in a list, using recursion."""
+
+    if lst == []:
+        return 0
+    
+    return 1 + count_recursively(lst[1:])
+
+print(count_recursively([])) #0
+print(count_recursively([1, 2, 3])) #3
+
+
+
+#3. Reverse Linked List 
+
+
+####################################################################
